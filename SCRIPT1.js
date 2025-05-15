401d6dc65f57f49dbbafd3f01d80b76616e594f2
AlejandroBZ7 <74073277@certus.edu.pe>
2025-05-14 23:29:15 -0500

    sesion viernes 09 mayo actu

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -1,149 +1,122 @@
 let VarGlobal1= "texto de variable global"
 
-function FCambioColor(){
-    let vartemporal= "texto temporal"
-    let vtexto1 = document.getElementById("ejmDOM1");
-    vTexto1.textContent= "texto cambiado por funcion FCambioColor()";
-    vTexto1.style.color= "red";
-    vTexto1.insertAdjacentHTML("beforeend",
-                            " <br> este texto es adicional <br>");
-    vTexto1.style.color= "green"
-
-    vTexto1.insertAdjacentHTML("beforeEnd", VarGlobal1);
+function FCambioColor() {
+    let vartemporal = "texto temporal";
+    let vTexto1 = document.getElementById("ejmDOM1");
+    vTexto1.textContent = "texto cambiado por funcion FCambioColor()";
+    vTexto1.style.color = "red";
+    vTexto1.insertAdjacentHTML("beforeend", ' <br><span style="color:green;">este texto es adicional</span><br>');
+    vTexto1.insertAdjacentHTML("beforeend", `<span style="color:green;">${VarGlobal1}</span>`);
     //la variable global se puede utilizar dentro de cualquier funcion
-
 }
+
 vTexto1="Texto fuera funcion"; //VTexto1 no es la variable de la funcion
-console.timeLog(vTexto1);
+console.log(vTexto1);
 //console.log(varTemporal); // varTemporal no esta definido afuera de funcion
 
-function FCambioClase(){
-    let vTexto2=document.getElementsByClassName("claseDOM1")
-    vTexto2(1).innerHTML= "Elemento [1] del arreglo de clase classDOM1";
-    vTexto2(1).style.color="white";
-    console.log("Tamaño del array",vTexto2.length);
-    console.log("tipo de variable vTexto2",typeof vTexto2[1]);
-// se coloca el indice 1 por ser el 2do elemento de la clase classDOM1 de la pag web     
-
+function FCambioClase() {
+    let vTexto2 = document.getElementsByClassName("claseDOM1");
+    if (vTexto2.length > 1) {
+        vTexto2[1].innerHTML = "Elemento [1] del arreglo de clase classDOM1";
+        vTexto2[1].style.color = "white";
+        console.log("Tamaño del array", vTexto2.length);
+        console.log("tipo de variable vTexto2", typeof vTexto2[1]);
+    }
 }
 
 function FCambioTag1() {
     let vTexto3 = document.getElementsByTagName('h2');
-    vTexto3[1].innerHTML = "Elemento h2 cambiado por TagDOM1";
-    vTexto3[10].innerHTML = "Elemento h2 cambiado por TagDOM1";
-    vTexto3[10].style.color="white" ; //"Elemento h2 cambiado por TagDOM1";
-    vTexto3[21].innerHTML = "Elemento h2 cambiado por TagDOM1";
-    // se coloca el índice 6 por ser el 7mo elemento h2 de la pag web index.html
+    if (vTexto3.length > 1) {
+        vTexto3[1].innerHTML = "Elemento h2 cambiado por TagDOM1";
+        vTexto3[1].style.color = "white";
+    }
 } 
 
-function FUsoFor1()
-{
-  const vTextoClass = document.getElementsByClassName("classFOR1");
-  var vTextoTotal="";
-  // como length=4 --> ira desde 0 hasta 3
-  for(let i=0 ; i< vTextoClass.length ; i++ )
-  {
-    vTextoClass[i].style.border ="2px solid red" ;
-    vTextoTotal=vTextoTotal + vTextoClass[i].innerText+'-';
-
-  } // fin de for
-  var nuevoH1 =document.createElement("h1");
-  var texto = document.createTextNode(vTextoTotal);
-  nuevoH1.appendChild(texto);
-  vTextoClass[3].appendChild(nuevoH1);
-  
+function FUsoFor1() {
+    const vTextoClass = document.getElementsByClassName("classFOR1");
+    var vTextoTotal = "";
+    for (let i = 0; i < vTextoClass.length; i++) {
+        vTextoClass[i].style.border = "2px solid red";
+        vTextoTotal = vTextoTotal + vTextoClass[i].innerText + '-';
+    }
+    var nuevoH1 = document.createElement("h1");
+    var texto = document.createTextNode(vTextoTotal);
+    nuevoH1.appendChild(texto);
+    if (vTextoClass.length > 3) {
+        vTextoClass[3].appendChild(nuevoH1);
+    }
 }  // fin de function
 
-function FEliminarConcat()
-{
-  const vTextoClass = document.getElementsByClassName("classFOR1");
-  let rpta =prompt("Estas seguro de eliminar el texto concatenado?(S/N)",'');
-  if (rpta=="S" || rpta=="s")  //  doble || significa o
-    { vTextoClass[3].remove();  // eliminar la class de posicion [3]
-      alert("CONCATENCION ELMINADA");
-    }  
-  else
-    { if(rpta=="N" || rpta=="n")
-      { alert("NO SE elimnará");
-      }
-      else
-      { alert("OPCION INCORRECTA-INgresa nuevamente");
-      }
+function FEliminarConcat() {
+    const vTextoClass = document.getElementsByClassName("classFOR1");
+    let rpta = prompt("Estas seguro de eliminar el texto concatenado?(S/N)", '');
+    if (rpta == "S" || rpta == "s") {
+        if (vTextoClass.length > 3) {
+            vTextoClass[3].remove();
+        }
+        alert("CONCATENCION ELMINADA");
+    } else if (rpta == "N" || rpta == "n") {
+        alert("NO SE elimnará");
+    } else {
+        alert("OPCION INCORRECTA-INgresa nuevamente");
     }
-  //removeChild funciona cuando un nodo(class, id, etx) esta dentro de otro
 }
 
-function FCapturarNombre(){
-
+function FCapturarNombre() {
     const vTexto1 = document.getElementById("nombre");
     const vTexto2 = document.querySelectorAll("input.datosPersonales");
     const vTexto3 = document.getElementById("clave");
-
-   // usar input para clases relacioandas a cajas de texto, etc
-    vTexto1.style.color="red";
-    
-    var valor1 =vTexto1.value; // value obtiene el contenido de la caja de texto nombre
-    var valor2 =""; //tipo string 
-    vTexto3.value=valor1; //asigna el nombre a la clave
-  
-    //foreach recorre todo el contenido del array de la clase datosPersonales
+    vTexto1.style.color = "red";
+    var valor1 = vTexto1.value;
+    var valor2 = "";
+    vTexto3.value = valor1;
     vTexto2.forEach(input => {
-        valor2 =valor2 +  `${input.name}: ${input.value} : ${input.id}    \n`;  // alt+96 
-        input.style.border="2px solid orange"      
-        //para comillas tipotexto permiten colocar variables del form y convertirlas a texto mediante $
-      });
- 
-      vTexto2[1].style.border="2px solid green"
+        valor2 = valor2 + `${input.name}: ${input.value} : ${input.id}    \n`;
+        input.style.border = "2px solid orange";
+    });
+    if (vTexto2.length > 1) {
+        vTexto2[1].style.border = "2px solid green";
+    }
     document.getElementById('observacion').value = valor2;
     console.log('El valor del nombre es:', valor1);
 }
 
-function FValidarCiclo()
-{
-  //var vaciclo =document.getElementById("Segundo");
-  const varciclo =document.getElementsByName("ciclo");
-
-  for(let i = 0; 1 < varciclo.length; i++) {
-    if (varciclo[1].checked=true) {
-      //alert(*Seleccionaste: " + varciclo[1].value);
-      document.getElementById('observacion').value = varciclo[1].value;
-      return; //Detener el bucle una vez que se encuentre el seleccionado  
+function FValidarCiclo() {
+    const varciclo = document.getElementsByName("ciclo");
+    for (let i = 0; i < varciclo.length; i++) {
+        if (varciclo[i].checked === true) {
+            document.getElementById('observacion').value = varciclo[i].value;
+            return;
+        }
     }
-  }
 }
 
-function FCrearDatosCiclo()
-{
-const radios = document.querySelectorAll('input[name"ciclo"]');
-
-radios.forEach(function(radio) {
-  radio.addEventListener('change', function(eventt) {
-   document.addEventListener("DOMContentLoaded", () => { 
-   console.leg("Ciclo seleccionado"; event.target.value);
-   const cantidad = event.target.value;
-  //event es el evento, target es el elemento radio, value es su valor del radio
-   // radio es elemnto radio
-   //
-   document.getElementById('observacion').value = cantidad;
-  })}
-  )}
- ) 
+function FCrearDatosCiclo() {
+    const radios = document.querySelectorAll('input[name="ciclo"]');
+    radios.forEach(function (radio) {
+        radio.addEventListener('change', function (event) {
+            const cantidad = event.target.value;
+            console.log("Ciclo seleccionado", cantidad);
+            document.getElementById('observacion').value = cantidad;
+        });
+    });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const numCole = document.getElementById("numColegios");
-  document.getElementById("numColegios").addEventListener("input",(event)=> {
-    let content ="";
-    const cantR=event.target.value;
-    const vTexto1 = document.getElementById("ejmDOM1");
-    vTexto1.innerText= cantR;   
-    document.getElementById('observacion').value = cantR;
-    for(let i=0; i<cantR ;i++)
-    {
-      content=content + `<div> <label> Colegio ${i+1} </label>
-                          <input type="text" id="cole[${i}" > </div>`;
+    const numCole = document.getElementById("numColegios");
+    if (numCole) {
+        numCole.addEventListener("input", (event) => {
+            let content = "";
+            const cantR = event.target.value;
+            const vTexto1 = document.getElementById("ejmDOM1");
+            if (vTexto1) vTexto1.innerText = cantR;
+            document.getElementById('observacion').value = cantR;
+            for (let i = 0; i < cantR; i++) {
+                content = content + `<div> <label> Colegio ${i + 1} </label>
+                          <input type="text" id="cole[${i}]" > </div>`;
+            }
+            document.getElementById("DetalleColegios").innerHTML = content;
+        });
     }
-  document.getElementById("DetalleColegios").innerHTML=content;
-  
-  });
-  });
+    FCrearDatosCiclo(); // Para que los radios funcionen desde el inicio
+});
