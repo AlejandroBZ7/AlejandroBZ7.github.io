5dc7103844a2ee3ffd495b2dafec6810abaf32f0
AlejandroBZ7 <74073277@certus.edu.pe>
2025-05-16 14:19:22 -0500

    sesion jueves 15 mayo actu

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -1,4 +1,4 @@
-let VarGlobal1= "texto de variable global"
+let VarGlobal1 = "texto de variable global"
 
 function FCambioColor() {
     let vartemporal = "texto temporal";
@@ -10,11 +10,11 @@ function FCambioColor() {
     //la variable global se puede utilizar dentro de cualquier funcion
 }
 
-vTexto1="Texto fuera funcion"; //VTexto1 no es la variable de la funcion
+vTexto1 = "Texto fuera funcion"; //VTexto1 no es la variable de la funcion
 console.log(vTexto1);
 //console.log(varTemporal); // varTemporal no esta definido afuera de funcion
 
-function FCambioClase() {
+function FCambioClase1() {
     let vTexto2 = document.getElementsByClassName("claseDOM1");
     if (vTexto2.length > 1) {
         vTexto2[1].innerHTML = "Elemento [1] del arreglo de clase classDOM1";
@@ -29,16 +29,64 @@ function FCambioTag1() {
     if (vTexto3.length > 1) {
         vTexto3[1].innerHTML = "Elemento h2 cambiado por TagDOM1";
         vTexto3[1].style.color = "white";
+        console.log("Tamaño del array=", vTexto3.length);
+        vTexto3[13].insertAdjacentHTML("beforeEnd", "<br>tamaño de h2 =" + vTexto3.length);
+        let totalh2 = vTexto3.length;
+        console.log("Valor de totalh2=", totalh2);
+        console.log("tipo de variable totalh2=", typeof totalh2);
     }
-} 
+}
+
+window.vTexto1 = "";
+
+function FAgregarTextoPermanente() {
+    let textoAdicional = "\nTexto adicional para concatenar";
+    vTexto1 = vTexto1 + textoAdicional;
+    console.log("tipo de variable de vTexto1 =", typeof vTexto1);
+    console.log(vTexto1);
+}
+
+function FcrearH1() {
+    var elemento = document.getElementById("idCrearH1");
+    var nuevoH1 = document.createElement("H1");
+    var texto = document.createTextNode("Texto colocado en linea");
+    nuevoH1.appendChild(texto);
+    elemento.appendChild(nuevoH1);
+
+}
+
+function FCapturarNombre() {
+    const vTexto1 = document.getElementById("nombre");
+    const vTexto2 = document.querySelectorAll("input.datosPersonales");
+    const vTexto3 = document.getElementById("clave");
+    // usar input para clases relacioandas a cajas de texto, etc
+    vTexto1.style.color = "red";
+
+    var valor1 = vTexto1.value; // value obtiene el contenido de la caja de texto nombre
+    var valor2 = ""; //tipo string 
+    vTexto3.value = valor1; // asigna el nombre a la clave
+
+
+    //foreach recorre todo el contenido del array de la clase datosPersonales
+    vTexto2.forEach(input => {
+        valor2 = valor2 + `${input.name}: ${input.value} : ${input.id}  \n`;  // alt+96 
+        input.style.border = "2px solid orange";
+        // para comillas tipotexto permiten colocar variables del form y convertirlas a texto mediante $
+    });
+
+    vTexto2[1].style.backgroundColor = "green";
+    document.getElementById('observacion').value = valor2;
+    console.log('El valor del nombre es:', valor1);
+}
 
 function FUsoFor1() {
     const vTextoClass = document.getElementsByClassName("classFOR1");
     var vTextoTotal = "";
+    // como length=4 --> ira desde 0 hasta 3
     for (let i = 0; i < vTextoClass.length; i++) {
         vTextoClass[i].style.border = "2px solid red";
         vTextoTotal = vTextoTotal + vTextoClass[i].innerText + '-';
-    }
+    } // fin de for
     var nuevoH1 = document.createElement("h1");
     var texto = document.createTextNode(vTextoTotal);
     nuevoH1.appendChild(texto);
@@ -50,9 +98,9 @@ function FUsoFor1() {
 function FEliminarConcat() {
     const vTextoClass = document.getElementsByClassName("classFOR1");
     let rpta = prompt("Estas seguro de eliminar el texto concatenado?(S/N)", '');
-    if (rpta == "S" || rpta == "s") {
+    if (rpta == "S" || rpta == "s") { //  doble || significa o
         if (vTextoClass.length > 3) {
-            vTextoClass[3].remove();
+            vTextoClass[3].remove(); // eliminar la class de posicion [rpta2]
         }
         alert("CONCATENCION ELMINADA");
     } else if (rpta == "N" || rpta == "n") {
@@ -60,31 +108,18 @@ function FEliminarConcat() {
     } else {
         alert("OPCION INCORRECTA-INgresa nuevamente");
     }
+    //removeChild funciona cuando un nodo(class, id, etx) esta dentro de otro
 }
 
-function FCapturarNombre() {
-    const vTexto1 = document.getElementById("nombre");
-    const vTexto2 = document.querySelectorAll("input.datosPersonales");
-    const vTexto3 = document.getElementById("clave");
-    vTexto1.style.color = "red";
-    var valor1 = vTexto1.value;
-    var valor2 = "";
-    vTexto3.value = valor1;
-    vTexto2.forEach(input => {
-        valor2 = valor2 + `${input.name}: ${input.value} : ${input.id}    \n`;
-        input.style.border = "2px solid orange";
-    });
-    if (vTexto2.length > 1) {
-        vTexto2[1].style.border = "2px solid green";
-    }
-    document.getElementById('observacion').value = valor2;
-    console.log('El valor del nombre es:', valor1);
-}
+
 
 function FValidarCiclo() {
+
+    //var varciclo =document.getElementById("Segundo");
     const varciclo = document.getElementsByName("ciclo");
     for (let i = 0; i < varciclo.length; i++) {
         if (varciclo[i].checked === true) {
+            //alert("Seleccionaste: " + varciclo[i].value);
             document.getElementById('observacion').value = varciclo[i].value;
             return;
         }
@@ -96,11 +131,30 @@ function FCrearDatosCiclo() {
     radios.forEach(function (radio) {
         radio.addEventListener('change', function (event) {
             const cantidad = event.target.value;
+            //event es el evento, target es el elemento radio, value es su valor del radio
+            // radio es el elemento radio
+            //
             console.log("Ciclo seleccionado", cantidad);
             document.getElementById('observacion').value = cantidad;
         });
     });
 }
+
+document.addEventListener("DOMContentLoaded", () => {  
+  const radios = document.querySelectorAll('input[name="ciclo"]');
+
+radios.forEach(function(radio) {
+   radio.addEventListener('change', function(event) {
+    console.log("Ciclo seleccionado:", event.target.value);
+    const cantidad = event.target.value;
+  //event es el evento, target es el elemento radio, value es su valor del radio
+   // radio es el elemento radio
+   //
+    document.getElementById('observacion').value = cantidad;
+  });
+}
+);
+  });
 
 document.addEventListener("DOMContentLoaded", () => {
     const numCole = document.getElementById("numColegios");
@@ -119,4 +173,85 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
     FCrearDatosCiclo(); // Para que los radios funcionen desde el inicio
+});
+
+function FVerificacion() {
+
+    let nombre1 =prompt("Ingresa tu primer nombre",'');
+    let nombre2 =prompt("Ingresa tu segundo nombre",'');
+    let apellido1 =prompt("Ingresa tu primer apellido",'');
+    let apellido2 =prompt("Ingresa tu segundo apellido",'');
+
+    let longitud1 = nombre1.length;
+    let longitud2 = nombre2.length;
+    let longitudapellido1 = apellido1.length;
+    let longitudapellido2 = apellido2.length;
+
+    let textoconcat="";
+
+        if( longitud1<longitud2 )
+ {
+
+  alert("El primer nombre:" +nombre1 + "es menor al segundo nombre:"+ nombre2)
+
+   textoconcat = nombre2 + nombre1; // concatenado y asignado a txtoconcat
+
+ }
+ else
+ { if(longitud1 > longitud2)
+     {
+      alert("El primer nombre:" +nombre1 + "es mayor al segundo nombre:"+ nombre2 )
+      textoconcat = nombre1 + nombre2; // concatenado y asignado a txtoconcat
+
+     }
+     else // caso por defecto igual
+     {
+       alert("El primer nombre:" +nombre1 + "es IGUAL en longitud al segundo nombre:"+ nombre2)
+       textoconcat = nombre1 + nombre2; // da lo mismo el orden del concatenado
+     }
+
+ }
+
+
+ ////conicionales para apellido
+ if( longitudapellido1 < longitudapellido2 )
+    {
+   
+     alert("El primer apellido:" +apellido11 + "es menor al segundo apellido:"+ apellido2)
+   
+      textoconcat = apellido2 + apellido1; // concatenado y asignado a txtoconcat
+   
+    }
+    else
+    { if(longitudapellido1 > longitudapellido2)
+        {
+         alert("El primer apellido:" +apellido1 + "es mayor al segundo apellido:"+ apellido2 )
+         textoconcat = apellido1 + apellido2; // concatenado y asignado a txtoconcat
+   
+        }
+        else // caso por defecto igual
+        {
+          alert("El primer apellido:" +apellido1 + "es IGUAL en longitud al segundo apellido:"+ apellido2)
+          textoconcat = apellido1 + apellido2; // da lo mismo el orden del concatenado
+        }
+   
+    }
+
+  alert("apellido concatenado :"+textoconcat);
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {    
+    const palabraInput = document.getElementById('palabra');
+    if (palabraInput) {
+        palabraInput.addEventListener('input', function(e) {
+            const contenedor = document.getElementById('cajas');
+            contenedor.innerHTML = '';
+            for (let i = 0; i < e.target.value.length; i++) {
+                const inputNum = document.createElement('input');
+                inputNum.type = 'number';
+                contenedor.appendChild(inputNum);
+            }
+        });
+    }
 });
